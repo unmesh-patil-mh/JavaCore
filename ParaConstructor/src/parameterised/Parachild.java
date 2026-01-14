e53022d6f4acadc910bd3e6491262df1b3503e2a
Unmesh Patil <punmesh56@gmail.com>
2026-01-14 21:28:02 +0530

    Add files via upload

---
+++ ParaConstructor/src/parameterised/Parachild.java
@@ -0,0 +1,11 @@
+package parameterised;
+
+public class Parachild {
+	Parachild(int a,int b){
+		System.out.println("The Substraction of the Number is: "+(a-b));
+	}
+	
+	public static void main(String[] args) {
+		Parachild p=new Parachild(2,5);
+	}
+}
---
+++ ParaConstructor/src/parameterised/Paraconst.java
@@ -0,0 +1,9 @@
+package parameterised;
+
+public class Paraconst {
+	Paraconst(int c,int d){
+		int a=c;
+		int b=d;
+		System.out.println("The Sum of The Numbers is: "+(c+d));
+	}
+}
