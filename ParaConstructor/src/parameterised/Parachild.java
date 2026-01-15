fa01ce9917bfda6ffffcfbc3dd007ffae60caf9f
Unmesh Patil <punmesh56@gmail.com>
2026-01-15 21:37:48 +0530

    Add files via upload

--- ParaConstructor/src/parameterised/Parachild.java
+++ ParaConstructor/src/parameterised/Parachild.java
@@ -1,11 +1,13 @@
 package parameterised;
 
-public class Parachild {
+public class Parachild extends Paraconst{
 	Parachild(int a,int b){
+		super(a,b);
 		System.out.println("The Substraction of the Number is: "+(a-b));
 	}
 	
 	public static void main(String[] args) {
-		Parachild p=new Parachild(2,5);
+		Parachild p=new Parachild(5,2);
+		
 	}
 }
--- ParaConstructor/src/parameterised/Paraconst.java
+++ ParaConstructor/src/parameterised/Paraconst.java
@@ -1,9 +1,9 @@
 package parameterised;
 
 public class Paraconst {
-	Paraconst(int c,int d){
-		int a=c;
-		int b=d;
-		System.out.println("The Sum of The Numbers is: "+(c+d));
+	Paraconst(int n1,int n2){
+		int a=n1;
+		int b=n2;
+		System.out.println("The Sum of The Numbers is: "+(a+b));
 	}
 }
