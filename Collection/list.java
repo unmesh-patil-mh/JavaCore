eb6872891a699710e2db568293483c4cb8092816
Unmesh Patil <punmesh56@gmail.com>
2026-01-19 23:22:25 +0530

    Add files via upload

---
+++ Collection/list.java
@@ -0,0 +1,15 @@
+package Collection;
+import java.util.List;
+import java.util.ArrayList;
+
+public class list {
+    public static void main(String[] args) {
+        List<Object> l = new ArrayList<>();
+        l.add(1);
+        l.add(2);
+        l.add("hii");
+        l.add("Welcome");
+        System.out.println(l);
+        
+    }
+}
